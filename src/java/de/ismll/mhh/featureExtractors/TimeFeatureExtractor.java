edbacb8b893e70b80d84510013715bbcf6c7f6dd
schilling <schilling@ismll.de>
2014-09-16 11:56:59 +0200

    added different orders in timefeatureextractor

--- src/java/de/ismll/mhh/featureExtractors/TimeFeatureExtractor.java
+++ src/java/de/ismll/mhh/featureExtractors/TimeFeatureExtractor.java
@@ -8,7 +8,9 @@ import de.ismll.table.projections.ColumnSubsetMatrixView;
 
 public class TimeFeatureExtractor {
 
-	public Matrix extractFeatures(Matrix data) {
+	public Matrix extractFeatures(Matrix data, int order) {
+		
+		// order = 1 -> gehe 1 nach links und rechts, order 2, dann gehe 2 nach links und rechts, usw...
 		
 		int nrCols = data.getNumColumns();
 		int nrRows = data.getNumRows();
@@ -18,11 +20,29 @@ public class TimeFeatureExtractor {
 		for (int feature = 0; feature < nrCols ; feature++) {
 			for (int instance = 0; instance < nrRows ; instance++) {
 				float value = 0;
-				if (instance == 0 || instance == (nrRows-1) ) {
+				if ( (instance-order+1) <= 0 || (instance+order-1) >= (nrRows-1) ) {
 					value = data.get(instance, feature);
 				}
 				else {
-					value = ( data.get(instance+1, feature) - data.get(instance-1, feature) )/2;
+					if (order == 1) {
+						value = ( data.get(instance+1, feature) - data.get(instance-1, feature) )/2;
+					}
+					else if (order ==2)  {
+						value = ( 1/12*data.get(instance-2, feature) -2/3*data.get(instance-1, feature) + 
+								2/3*data.get(instance+1, feature) + 1/12*data.get(instance+2, feature));
+					}
+					else if (order == 3) {
+						value = ( -1/60*data.get(instance-3, feature) + 3/20*data.get(instance-2, feature) +  -3/4*data.get(instance-1, feature) 
+								+ 3/4*data.get(instance+1, feature)
+								- 3/20*data.get(instance+2, feature) + 1/60*data.get(instance+3, feature));
+					}
+					else if (order ==4) {
+						value = (1/280*data.get(instance-4, feature) -4/105*data.get(instance-3, feature)
+								+ 1/5*data.get(instance-2, feature) +  -4/5*data.get(instance-1, feature) 
+								+ 4/5*data.get(instance+1, feature)
+								- 1/5*data.get(instance+2, feature) + 4/105*data.get(instance+3, feature) - 1/280*data.get(instance+4, feature));
+					}
+					
 				}
 				ret.set(instance, feature, value);
 			}
