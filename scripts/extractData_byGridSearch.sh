8c68d5e62fc9bc378829762e28c23b231aac6b98
Andre Busche <andre.busche@gmx.de>
2016-06-28 16:13:13 +0200

    merge configuration and script

--- scripts/extractData_byGridSearch.sh
+++ scripts/extractData_byGridSearch.sh
@@ -2,20 +2,37 @@
 
 #Fill in the following fields to design a hyperSearch Script!!
 
-# try to source in a configuration
-. extractData.properties
+# read the references and functions from this file
+. readProplib.sh
+
+# read the properties from file mhh.general.properties
+sourcein 'general' mhh.general.properties
+
+#
+# the path section
+#
+# where is the data read from and stored to?
+#
+
+# which split directory to use?
+usesplitsdir=${splitsdir?Warning! Could not get the base splitsdir}/
+
+# which columns to use?
+columnselector="33,166"
+
+includeRD=false
+
+annotator="sm"
 
 u_bootstrapclass=de.ismll.secondversion.PreprocessData
 u_splitsdir=${usesplitsdir?Error: no experimentsplitsdir found}
 
-u_annotator=${annotator?Error: no annotator found!}
-u_includeRD=${includeRD?Error: no includeRD found!}
-u_columnselector=${columnselector?Error: non columnselector found!}
-
 ./run.sh ${u_bootstrapclass} \
 splitFolder=${u_splitsdir}  \
-annotator=${u_annotator} \
-columnSelector="${u_columnselector}" \
+annotator=${annotator} \
+columnSelector="${columnselector}" \
 annotationBaseDir=${annotationsbasedir}/NormalAnnotations/ \
-includeRD=${u_includeRD} \
-serializationOutput=/tmp
+includeRD=${includeRD} \
+serializationOutput=/tmp/columns_${columnselector}_includeRD_${includeRD}_annotator_${annotator}_normalannotations
+
+
